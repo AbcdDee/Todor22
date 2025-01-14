50c71e1aa704c1de3aacfcd431a836eadc1a1600
AbcdDee <godparticle123321@gmail.com>
2025-01-14 22:12:07 +0530

    todo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Todo from './Todo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Todo List</h1>
+      </header>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
